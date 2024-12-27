e9c78ecfd862a41b646553ec7952616e04b44913
Mradul (Intern) <mradul@nousinfo.com>
2024-12-27 11:19:10 +0530

    first commit

---
+++ server.py
@@ -0,0 +1,20 @@
+import google.generativeai as genai
+import csv
+import json
+from flask import Flask,request, render_template
+import os
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')  # The HTML form
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    
+    return 'Sucessefull response', 200
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
